4cf346f9aa2a36a3d62f08fa31d6c111dda78153
lathagopal <43037276+lathagopal@users.noreply.github.com>
2019-02-13 14:07:10 +0530

    Create primenumbers.py

---
+++ primenumbers.py
@@ -0,0 +1,14 @@
+def main():
+  a= int(input(" "))
+ b= int(input(""))
+ for num in range(a,b + 1):
+   
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
+
+if __name__ == '__main__':
+    main()
