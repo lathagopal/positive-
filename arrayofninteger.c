a516bc753d37e19f3ac8611c00f0d8b399e8ac61
lathagopal <43037276+lathagopal@users.noreply.github.com>
2019-02-20 14:05:50 +0530

    Create arrayofninteger.c

---
+++ arrayofninteger.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) {
+	int x,i,temp;
+	scanf("%d",&x);
+	int a[x];
+	for(i=0;i<x;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<x;i++)
+	{
+		
+		
+			if(a[i]>a[i+1])
+			{
+				temp=a[i];
+				a[i]=a[i+1];
+				a[i+1]=temp;
+			}
